d2025f657e2f0cab8f74aa6986fd4f2ddb471ac5
beautifulBoys <1298947916@qq.com>
2017-02-11 19:03:30 +0800

    2-11

---
+++ src/components/login/login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="app">
+        <li-load @loadFinishEvent="loadEvent"></li-load>
+        <div style="width: 100%;height: 45%;"></div>
+        <div class="loading">正在加载群聊信息...</div>
+    </div>
+</template>
+
+<script>
+    import load from '../loading';
+    import header from '../header';
+    export default {
+        components: {
+            'li-header': header,
+            'li-load': load
+        },
+        data () {
+            return {
+                load: false
+            };
+        },
+        methods: {
+            loadEvent () {
+                this.$router.replace({path: '/room'});
+            }
+        }
+    };
+</script>
+
+<style lang="less" scoped>
+    .app {
+        width: 100%;
+        height: 100%;
+        background: #fff;
+        .loading {
+            width: 100%;
+            height: 100%;
+            font-size: 14px;
+            color: #499eff;
+            font-family: "Microsoft YaHei";
+            text-align: center;
+        }
+    }
+</style>
